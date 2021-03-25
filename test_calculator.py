5be751161d86a6009b48b1d16643c28825dfc4e8
ethanbhm <ethan.bahmanyari@otto.de>
2021-03-25 19:13:50 +0100

    Initial commit

---
+++ test_calculator.py
@@ -0,0 +1,43 @@
+import calculator
+import unittest
+
+
+class CalculatorTests(unittest.TestCase):
+
+    def setUp(self):
+        self.calculator = calculator.Calculator()
+
+    def test_add_method(self):
+        result = self.calculator.add(4, 2)
+        self.assertEqual(6, result)
+
+    def test_add_method_invalid_value(self):
+        self.assertRaises(ValueError, self.calculator.add, "four", "five")
+
+    def test_multiply_method(self):
+        result = self.calculator.multiply(5, 3)
+        self.assertEqual(15, result)
+
+    def test_multiply_method_invalid_value(self):
+        self.assertRaises(ValueError, self.calculator.multiply, "four", "five")
+
+    def test_sub_method(self):
+        result = self.calculator.sub(6, 4)
+        self.assertEqual(2, result)
+
+    def test_sub_method_invalid_value(self):
+        self.assertRaises(ValueError, self.calculator.sub, "four", "five")
+
+    def test_div_method(self):
+        result = self.calculator.div(5, 1)
+        self.assertEqual(5, result)
+
+    def test_div_method_invalid_value(self):
+        self.assertRaises(ValueError, self.calculator.div, "five", "four")
+
+    def test_div_method_zero(self):
+        self.assertRaises(ZeroDivisionError, self.calculator.div, 5, 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
